284221ddedb6b1197a003e57dd792558f483816a
andriagov <101366707+andriagov@users.noreply.github.com>
2022-06-06 14:51:33 +1200

    Add files via upload

--- assignment-may-2022/assets/main.js
+++ assignment-may-2022/assets/main.js
@@ -3,17 +3,17 @@
 
 function printValues() {
     // gets the values the user has entered in the inputs
-    const name = document.querySelector('[data-name]').value
-    const date = document.querySelector('[data-date]').value
+    const name = document.querySelector('[data-name]').value;
+    const date = document.querySelector('[data-date]').value;
     // finds the list and creates a list element
-    const ul = document.querySelector('[data-output]')
-    const li = document.createElement('li')
+    const ul = document.querySelector('[data-output]');
+    const li = document.createElement('li');
     // assign class to list items so css styling applies
     li.classList.add('list-items');
     // list item will contain texting saying "(name entered) was here on (date entered)"
-    li.textContent = name + ' was here on ' + date
+    li.textContent = name + ' was here on ' + date;
     // adds the list item to the unordered list
-    ul.appendChild(li)
+    ul.appendChild(li);
     // clear inputs
     document.getElementById("name").value = "";
     document.getElementById("date").value = "";
